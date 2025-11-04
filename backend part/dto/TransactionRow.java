50a2d256eb24f5bb054e81f3821a32f741383701
Kakumanu Venu Alekhya <123382901+alekhya0912@users.noreply.github.com>
2025-11-04 09:44:14 +0530

    Create TransactionRow.java

---
+++ backend part/dto/TransactionRow.java
@@ -0,0 +1,72 @@
+package paymentinitiationbackend.dto;
+
+
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class TransactionRow {
+    private LocalDateTime date;
+    private String id;
+    private String employee;
+    private String account;
+    private BigDecimal amount;
+    private String status;
+    private Long employeeId;
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(String employee) {
+        this.employee = employee;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}

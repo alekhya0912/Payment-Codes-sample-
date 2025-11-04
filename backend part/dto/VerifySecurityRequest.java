fb7d6d65712b65ce581559d3b3a07eb3094e65f8
Kakumanu Venu Alekhya <123382901+alekhya0912@users.noreply.github.com>
2025-11-04 09:45:32 +0530

    Create VerifySecurityRequest.java

---
+++ backend part/dto/VerifySecurityRequest.java
@@ -0,0 +1,40 @@
+package paymentinitiationbackend.dto;
+
+public class VerifySecurityRequest {
+    private String userId;
+    private String mobileNumber;
+    private String securityQuestion;
+    private String securityAnswer;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getSecurityQuestion() {
+        return securityQuestion;
+    }
+
+    public void setSecurityQuestion(String securityQuestion) {
+        this.securityQuestion = securityQuestion;
+    }
+
+    public String getSecurityAnswer() {
+        return securityAnswer;
+    }
+
+    public void setSecurityAnswer(String securityAnswer) {
+        this.securityAnswer = securityAnswer;
+    }
+}
